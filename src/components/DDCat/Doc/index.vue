6ba260e5ab9026fe2c0b619788055f4ab76779db
张有良 <17639627374@163.com>
2021-06-15 20:44:17 +0800

    feat: first commit

---
+++ src/components/DDCat/Doc/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <svg-icon icon-class="question" @click="goto"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DDCatDoc',
+  data() {
+    return {
+      url: 'https://nextjs-docs-henna.vercel.app/'
+    }
+  },
+  methods: {
+    goto() {
+      window.open(this.url)
+    }
+  }
+}
+</script>
---
+++ src/components/DDCat/Git/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <svg-icon icon-class="github" @click="goto"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DDCatGit',
+  data() {
+    return {
+      url: 'https://gitee.com/dd-cat/ddcat-ui.git'
+    }
+  },
+  methods: {
+    goto() {
+      window.open(this.url)
+    }
+  }
+}
+</script>
